96350ee7a302f23ffe3fc6e6fffa7aac70ad5161
Benn <benngraham90@gmail.com>
2025-04-02 10:12:55 -0400

    add http client provider, basic list for mission display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { MissionListComponent } from "./missionlist/missionlist.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [CommonModule, RouterOutlet, MissionListComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = '101412278-lab-test2-comp3133';
+  title = "101412278-lab-test2-comp3133";
 }
--- src/app/missionlist/missionlist.component.ts
+++ src/app/missionlist/missionlist.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Mission } from "../models/mission";
+import { SpacexapiService } from "../network/spacexapi.service";
 
 @Component({
-  selector: 'app-missionlist',
-  imports: [],
-  templateUrl: './missionlist.component.html',
-  styleUrl: './missionlist.component.css'
+  selector: "app-missionlist",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./missionlist.component.html",
+  styleUrls: ["./missionlist.component.css"],
 })
-export class MissionlistComponent {
+export class MissionListComponent implements OnInit {
+  missions: Mission[] = [];
+  loading = true;
+  error: string | null = null;
 
+  constructor(private spacexService: SpacexapiService) {}
+
+  ngOnInit(): void {
+    this.spacexService.getMissions().subscribe((data: any) => {
+      console.log(data);
+      this.missions = data;
+    });
+  }
 }
