420900ed2eb667354e2e35cc1322919e1f06eb0f
Benn <benngraham90@gmail.com>
2025-04-02 12:26:50 -0400

    styled material grid for mission list, add success/failure indicator to grid cards

--- src/app/missionlist/missionlist.component.ts
+++ src/app/missionlist/missionlist.component.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { Mission } from '../models/mission';
 import { SpacexapiService } from '../network/spacexapi.service';
 import { MissiondetailsComponent } from '../missiondetails/missiondetails.component';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-missionlist',
   standalone: true,
-  imports: [CommonModule, MissiondetailsComponent],
+  imports: [
+    CommonModule,
+    MissiondetailsComponent,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+  ],
   templateUrl: './missionlist.component.html',
   styleUrls: ['./missionlist.component.css'],
 })
@@ -18,6 +26,7 @@ export class MissionListComponent implements OnInit {
   yearOptions = new Date().getFullYear() - 2005;
   selectedMission: Mission | null = null;
   showDetails = false;
+  numCols: number = 3;
 
   years: number[] = Array.from(
     { length: this.yearOptions },
@@ -27,6 +36,16 @@ export class MissionListComponent implements OnInit {
   constructor(private spacexService: SpacexapiService) {}
 
   ngOnInit(): void {
+    const width = window.innerWidth;
+    if (width <= 600) {
+      this.numCols = 2;
+    } else if (width <= 960) {
+      this.numCols = 3;
+    } else if (width <= 1280) {
+      this.numCols = 4;
+    } else {
+      this.numCols = 5;
+    }
     this.loadMissions();
   }
 
@@ -52,5 +71,18 @@ export class MissionListComponent implements OnInit {
   hideModal(): void {
     this.showDetails = false;
     this.selectedMission = null;
+  }
+
+  onResize(event: any) {
+    const width = event.target.innerWidth;
+    if (width <= 600) {
+      this.numCols = 2;
+    } else if (width <= 960) {
+      this.numCols = 3;
+    } else if (width <= 1280) {
+      this.numCols = 4;
+    } else {
+      this.numCols = 5;
+    }
   }
 }
